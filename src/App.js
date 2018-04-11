53dc2323fbc680f79b175ea78a55f40b04832bac
Akshay <akshay365@icloud.com>
2018-04-10 22:08:37 -0700

    React Crypto currency
    This React app shows the current pricing for bitcoin, etherium and iota

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+var NumberFormat = require('react-number-format');
+
+class App extends Component {
+
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH&tsyms=USD')
+    .then(res => {
+      const cryptos = res.data;
+      console.log(cryptos);
+      this.setState({cryptos: cryptos})
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {Object.keys(this.state.cryptos).map((key) => (
+
+          <div id = "crypto-container">
+          <span className = "left">{key}</span>
+          <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></span>
+
+
+
+          </div>
+
+         ) )}
+      </div>
+    );
+  }
+}
+
+export default App;
