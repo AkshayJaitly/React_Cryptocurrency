b6f5aa145b74173d0f9dc29546dd1035c784c950
Akshay <akshay365@icloud.com>
2020-07-26 16:13:48 -0500

    Updates

---
+++ precache-manifest.39fe29964dc5fac514b2c027f5839775.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec188002d85e06f6c060e8d154627b33",
+    "url": "/AkshayJaitly/React_Cryptocurrency/index.html"
+  },
+  {
+    "revision": "c0f064165b1f0cb71ba8",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/css/main.f7c3764d.chunk.css"
+  },
+  {
+    "revision": "65dad3bf576cbe539d21",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/js/2.db730e0e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/js/2.db730e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f064165b1f0cb71ba8",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/js/main.3c0282c1.chunk.js"
+  },
+  {
+    "revision": "8fba330dab7bbe270af9",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/js/runtime-main.396a5b31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AkshayJaitly/React_Cryptocurrency/static/media/logo.5d5d9eef.svg"
+  }
+]);
